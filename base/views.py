31183c2dbca48d14c525e693ce10ba5ba227786c
Abhinav <vanihba.arora@gmail.com>
2022-12-06 19:32:15 +0530

    import models in views file, query statements to access the database; all() - gets all rows, get(id=pk) - gets row by id; views then pass data as context to templates; data is then accessed in templates for displaying as html

--- base/views.py
+++ base/views.py
@@ -1,22 +1,24 @@
 from django.shortcuts import render
 
-rooms = [
-    {'id': 1, 'name': 'Room 1'},
-    {'id': 2, 'name': 'Room 2'},
-    {'id': 3, 'name': 'Room 3'},
-]
+from .models import Room
+
+# queryset = ModelName.objects.all() # objects is the model manager which gives access to methods like all(), get(), filter(), exclude()
+
+# rooms = [
+#     {'id': 1, 'name': 'Room 1'},
+#     {'id': 2, 'name': 'Room 2'},
+#     {'id': 3, 'name': 'Room 3'},
+# ]
 
 
 def home(request):
+    rooms = Room.objects.all()  # query to get all rooms. objects is the model manager
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)  # query data using id
     context = {'room': room}
     return render(request, 'base/room.html', context)
 
