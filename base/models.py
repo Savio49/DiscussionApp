3a2d5eb8535b075928cf78dbeaec68e20e9799f9
Abhinav <vanihba.arora@gmail.com>
2022-12-06 18:37:08 +0530

    CHECKPOINT: Working with Sqlite databse, migrating django default models, creating new model with attributes, preparing migration and migration of your new model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+# class name = Database name
+# attributes of class = columns of database
+# every instance of class = row of database
+
+# default models of django are mentioned in settings.py
+# set them up using # python manage.py migrate
+
+
+class Room(models.Model):
+    # host
+    # topic
+    name = models.CharField(max_length=200)
+    # null=True means description can be blank, blank=True means when form is saved, this field can be empty
+    description = models.TextField(null=True, blank=True)
+    # participants =
+    # takes time snapshot of anytime this model instance was updated i.e whenever the save method was run to update this model. auto_now means Django automatically takes a time stamp when save method is run
+    updated = models.DateTimeField(auto_now=True)
+    # takes snapshot only the first time when model was created
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+# models are classes which make up the database
+# migration means converting a model into a database
+# prepare migrations using: # python manage.py makemigrations
+# migrate using: # python manage.py migrate
+# access the database using either third party software like sqlitestudio or django built-in admin portal
+# create super-user to access the django admin page # python manage.py createsuperuser
+# user: savio49
+# password: P@ssw0rd@1234
--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    # url-name= room which will always point to 'room/<str:pk>/'
     path('room/<str:pk>/', views.room, name="room"),
 ]
