1a1221b65d80b9e260ee575a1db86322e2c4da31
Abhinav <vanihba.arora@gmail.com>
2022-12-06 17:55:14 +0530

    using url-names to access urls in the html as variables

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    # url-name= room which will always point to 'room/<str:pk>/'
     path('room/<str:pk>/', views.room, name="room"),
 ]
