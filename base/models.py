a0fda68070af80d017eb39f00fd18c1398c331b4
Abhinav <vanihba.arora@gmail.com>
2022-12-06 20:17:47 +0530

    Adding new models, establishing relationships between models; makemigrations, migrate

--- base/models.py
+++ base/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 # class name = Database name
 # attributes of class = columns of database
@@ -9,9 +9,18 @@ from django.db import models
 # set them up using # python manage.py migrate
 
 
+# Topics are super-fields of Rooms, i.e. each room has only one topic, but a topic can have multiple rooms.
+class Topic(models.Model):
+    # Place it above Room model in order to access it in Room
+    name = models.CharField(max_length=200)
+
+    def __string__(self):
+        return self.name
+
+
 class Room(models.Model):
     # host
-    # topic
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     # null=True means description can be blank, blank=True means when form is saved, this field can be empty
     description = models.TextField(null=True, blank=True)
@@ -26,12 +35,16 @@ class Room(models.Model):
 
 
 class Message(models.Model):
-    # user
+    # django already has a built in user-model. User can have many messages, but each message has only one user. When a user is deleted, the messages are deleted
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     # one to many relationship: a user can have many messages. Foreign key in databases. CASCADE deletes all the messages in the room if the room is deleted. SET_NULL sets the message values to null on room deletion
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField()
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[0:50]
 
 
 # models are classes which make up the database
