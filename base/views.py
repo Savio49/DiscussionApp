9fd00034d132302ec896aed4420522e2674637ad
Abhinav <vanihba.arora@gmail.com>
2022-12-09 16:49:28 +0530

    Using QLookup inside objects.filter() to access | and & operators for advanced searches. Also displaying number of items shown by the query

--- base/views.py
+++ base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 
 from .models import Room, Topic
 from .forms import RoomForm
+from django.db.models import Q
 
 # queryset = ModelName.objects.all() # objects is the model manager which gives access to methods like all(), get(), filter(), exclude()
 
@@ -11,12 +12,21 @@ def home(request):
     # q = request.GET.get('q')
     # rooms = Room.objects.filter(topic__name=q) # Doesn't work for 'All'
 
+    # 'q' is the name of input form in navbar.html
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     # filter works if the query contains atleast some part of the string. i makes the case-insensitive. other parameters which may be used: startswith, endswith etc
-    rooms = Room.objects.filter(topic__name__icontains=q)
+    # rooms = Room.objects.filter(topic__name__icontains=q) # This only searches by topic__name. Not by user or room name. How to make it dynamic?
+    # rooms = Room.objects.filter(topic__name__icontains=q, name__contains=q) # Not a good way. Instead use django's QLookup method to add AND/OR statements in searches
+    rooms = Room.objects.filter(Q(topic__name__icontains=q) |
+                                Q(name__icontains=q) |
+                                Q(description__icontains=q) |
+                                Q(host__username__icontains=q)
+                                )
 
+    # alternatively use len(rooms). But this works faster cuz specially for sql queries
+    room_count = rooms.count()
     topics = Topic.objects.all()
-    context = {'rooms': rooms, 'topics': topics}
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
     return render(request, 'base/home.html', context)
 
 
