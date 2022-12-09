bd226a67e1f5012ff051ad5cbab5e5df51a8c41b
Abhinav <vanihba.arora@gmail.com>
2022-12-09 20:11:53 +0530

    Prepping for user login page. adding login template(common for login and register). adding an empty view. adding a url in urls.py. showing the login link in navbar.

--- base/views.py
+++ base/views.py
@@ -4,26 +4,21 @@ from .models import Room, Topic
 from .forms import RoomForm
 from django.db.models import Q
 
-# queryset = ModelName.objects.all() # objects is the model manager which gives access to methods like all(), get(), filter(), exclude()
+
+def loginPage(request):
+    context = {}
+    return render(request, 'base/login_register', context)
 
 
 def home(request):
-    # .filter() works like .all(), topic__name comes from attributes of Room model. That works but except for 'All' filter
-    # q = request.GET.get('q')
-    # rooms = Room.objects.filter(topic__name=q) # Doesn't work for 'All'
 
-    # 'q' is the name of input form in navbar.html
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    # filter works if the query contains atleast some part of the string. i makes the case-insensitive. other parameters which may be used: startswith, endswith etc
-    # rooms = Room.objects.filter(topic__name__icontains=q) # This only searches by topic__name. Not by user or room name. How to make it dynamic?
-    # rooms = Room.objects.filter(topic__name__icontains=q, name__contains=q) # Not a good way. Instead use django's QLookup method to add AND/OR statements in searches
     rooms = Room.objects.filter(Q(topic__name__icontains=q) |
                                 Q(name__icontains=q) |
                                 Q(description__icontains=q) |
                                 Q(host__username__icontains=q)
                                 )
 
-    # alternatively use len(rooms). But this works faster cuz specially for sql queries
     room_count = rooms.count()
     topics = Topic.objects.all()
     context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
@@ -39,7 +34,6 @@ def room(request, pk):
 def createRoom(request):
     form = RoomForm()
     if request.method == 'POST':
-        # print(request.POST)
         form = RoomForm(request.POST)
         if form.is_valid():
             form.save()
