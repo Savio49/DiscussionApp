a25f45a29c10c93dbc7d0a13aaf4474d24bd1cba
Abhinav <vanihba.arora@gmail.com>
2022-12-11 01:45:40 +0530

    Added 'participants' column/field to the Room model, makemigrations, migrate

--- base/models.py
+++ base/models.py
@@ -14,7 +14,9 @@ class Room(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    # participants =
+    # need to specify related_name bcz User model already connected to host field
+    participants = models.ManyToManyField(
+        User, related_name='participants', blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
