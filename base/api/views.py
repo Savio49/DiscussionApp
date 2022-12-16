b71364a8f117d5ba4ac448b77a35c1b5683f17ee
Abhinav <vanihba.arora@gmail.com>
2022-12-16 20:33:11 +0530

    Using djangorestframework to display basic response, serialize rooms data and displaying in api/rooms

--- base/api/views.py
+++ base/api/views.py
@@ -1,10 +1,28 @@
-from django.http import JsonResponse
+# from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from base.models import Room
+
+from .serializers import RoomSerializer
 
 
+@api_view(['GET'])  # list of methods that will be accessible to api users
 def getRoutes(request):
     routes = [
         'GET /api',
         'GET /api/rooms',
         'GET /api/rooms/:id',
     ]
-    return JsonResponse(routes, safe=False)
+    # python dictionaries, lists can be serialized automatically but not objects
+    return Response(routes)
+
+
+@api_view(['GET'])
+def getRooms(request):
+    page = 'rooms'
+
+    rooms = Room.objects.all()
+    # many=True allows multiple objects for serialization
+    serializer = RoomSerializer(rooms, many=True)
+
+    return Response(serializer.data)
