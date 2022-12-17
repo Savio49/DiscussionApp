6609a53e5c08a18de6b98d3a568ea6aaccc7bded
Abhinav <vanihba.arora@gmail.com>
2022-12-17 16:26:53 +0530

    update static path in settings IX

--- studybud2/settings.py
+++ studybud2/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 STATIC_ROOT = os.path.join(BASE_DIR ,'staticfiles')
@@ -137,7 +137,7 @@ STATICFILES_DIRS = [
 ]
 
 #For user uploaded images: (Tell django where to store user uploaded images)
-MEDIA_ROOT = BASE_DIR / 'static/images' # Use AWS S3 bucket for production environment
+MEDIA_ROOT = os.path.join(BASE_DIR,'static/images') # Use AWS S3 bucket for production environment
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
