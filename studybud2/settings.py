334da775ebf1beb93e08a4af8955d51c0243e20a
Abhinav <vanihba.arora@gmail.com>
2022-12-17 11:37:51 +0530

    Profile picture functionality added, but can only be accessed from admin panel

--- studybud2/settings.py
+++ studybud2/settings.py
@@ -128,10 +128,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+#For user uploaded images: (Tell django where to store user uploaded images)
+MEDIA_ROOT = BASE_DIR / 'static/images' # Use AWS S3 bucket for production environment
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
